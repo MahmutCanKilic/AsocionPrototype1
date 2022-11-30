0b9055f4633fb079c7f1658abe840be0c6a4d0b5
Murphyqq <89387909+Murphyqq@users.noreply.github.com>
2022-12-01 01:11:05 +0300

    Small Changes

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    private float maxSpawnObject;
-
+    [SerializeField] private GameObject[] spawnLine;
+    [SerializeField] private GameObject[] spawnPoints;
+    [SerializeField] private GameObject pickable;
+    float countFuel;
+    float maxFuel = 5;
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+        SpawnPickable();
+
+    }
+    private void SpawnPickable()
+    {
+       /* if (countFuel <= maxFuel)
+        {
+            int spawnLineNumber = Random.Range(0, spawnLine.Length);
+            int spawnNumber = Random.Range(0, spawnPoints.Length);
+            GameObject pickable = Instantiate(spawnPoints[0], spawnLine[spawnLineNumber].transform.position, Quaternion.identity);
+            gameObject.transform.SetParent(pickable.transform);
+            countFuel++;
+        }*/
+
+
+
     }
 }
